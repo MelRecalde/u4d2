acb1a4b2b819ae0c02169f6a24b2e837720534d7
MelRecalde <melquisedicrecalde@gmail.com>
2024-07-23 16:49:21 +0200

    repository

---
+++ src/Es1.java
@@ -0,0 +1,9 @@
+public class Es1 {
+    public static void main(String[] args) {
+    }
+
+    public static boolean evenOdd(String txt) {;
+        if (txt.length() % 2 != 0) return true;
+        else return false;
+    }
+}
---
+++ src/Es2.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Es2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
